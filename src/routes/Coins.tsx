9c17ab220063e9aa29dc357a526f838a37399889
ohnas <ohnas12@gmail.com>
2023-03-23 12:20:52 +0900

    3.4 Route States

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -23,6 +23,8 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
     display: block;
@@ -41,6 +43,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -75,7 +83,10 @@ function Coins() {
                     <CoinsList>
                         {coins.map((coin) => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link to={{pathname:`/${coin.id}`, state: {name : coin.name}}}>
+                                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} />
+                                {coin.name} &rarr;
+                            </Link>
                         </Coin>
                         ))}
                     </CoinsList>
