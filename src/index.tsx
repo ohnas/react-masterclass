1941733a052dd2c354c14b5cd2d16e2131baf6ac
ohnas <ohnas12@gmail.com>
2023-03-23 11:52:04 +0900

    3.3 Home part Two

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <div>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </div>
 );
