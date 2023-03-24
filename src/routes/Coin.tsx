df4d9bf261f8b84a7478a491f6b67d996000f05c
ohnas <ohnas12@gmail.com>
2023-03-24 11:27:25 +0900

    3.15 Final Touches

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { Link, Route, Switch, useLocation, useParams, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
 import { fetchCoinInfo, fetchCoinTickers } from "../api";
 import Chart from "./Chart";
 import Price from "./Price";
@@ -147,11 +148,19 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
+    () => fetchCoinTickers(coinId),
+    {
+      // refetchInterval: 5000,
+    }
   );
   const loading = infoLoading || tickersLoading;
   return (
       <Container>
+          <Helmet>
+            <title>
+              {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+            </title>
+          </Helmet>
           <Header>
             <Title>
               {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -171,8 +180,8 @@ function Coin() {
                   <span>${infoData?.symbol}</span>
                 </OverviewItem>
                 <OverviewItem>
-                  <span>Open Source:</span>
-                  <span>{infoData?.open_source ? "Yes" : "No"}</span>
+                  <span>Price:</span>
+                  <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
                 </OverviewItem>
               </Overview>
               <Description>{infoData?.description}</Description>
