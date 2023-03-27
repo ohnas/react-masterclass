fbd46783c47bdc77993b4f5d0f2275e5a75fb368
ohnas <ohnas12@gmail.com>
2023-03-27 14:29:58 +0900

    4.5 To Do Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from "./theme";
 import App from './App';
 
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <div>
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>
         <App />
-      </QueryClientProvider>
+      </ThemeProvider>
     </RecoilRoot>
   </div>
 );
